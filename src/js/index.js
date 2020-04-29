441c786b1557159f37a42baa4017aae433d7032a
Andrea Ramirez <anlramirezs11@gmail.com>
2020-04-28 23:03:28 -0500

    Se termina de configurar el segundo menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,68 +1,38 @@
 'use strict';
 import data from "./data.js";
-
-const $all = document.getElementById('all');
-const $branding = document.getElementById('branding');
-const $web = document.getElementById('web');
-const $photography = document.getElementById('photography');
-const $app = document.getElementById('app');
 const $portoflio = document.getElementById('portoflio');
 const $button = document.getElementById("mybutton");
 const $menu = document.getElementsByClassName("menuLink");
 
-
-
 OnInit();
-
 function OnInit() {
     loadData(data, "all");
-
-    $all.addEventListener("click", () => {
-        loadData(data, "all");
-        for(let i=0; i<$menu.length ;i++){
-            $menu[i].classList.remove('active');
-        }        
-        $all.classList.add('active');
-    });
-
-    $branding.addEventListener("click", function () {
-        loadData(data, "branding");
-        for(let i=0; i<$menu.length ;i++){
-            $menu[i].classList.remove('active');
-        }        
-        $branding.classList.add('active');
-    });
-
-    $web.addEventListener("click", function () {
-        loadData(data, "web");
-        for(let i=0; i<$menu.length ;i++){
-            $menu[i].classList.remove('active');
-        }        
-        $web.classList.add('active');
-    });
-
-    $photography.addEventListener("click", function () {
-        loadData(data, "photography");
-        for(let i=0; i<$menu.length ;i++){
-            $menu[i].classList.remove('active');
-        }        
-        $photography.classList.add('active');
-    });
-
-    $app.addEventListener("click", function () {
-        loadData(data, "app");
-        for(let i=0; i<$menu.length ;i++){
-            $menu[i].classList.remove('active');
-        }        
-        $app.classList.add('active');
-    });
-
-    $button.onclick = function () {
+    for (let i = 0; i < $menu.length; i++) {
+        $menu[i].addEventListener('click', changeMenu);
+    }
+    $button.onclick = () => {
         window.scroll(0, 500)
     }
 
 };
 
+function changeMenu(e) {
+    let idAux = this.id;
+    for (let i = 0; i < $menu.length; i++) {
+        $menu[i].classList.remove('active');
+    }
+
+    if (this.id.endsWith('-1')) {
+        idAux = this.id.substr(0, this.id.length - 2);
+        console.log(document.getElementById(this.id))
+        document.getElementById(idAux).classList.add('active');
+        document.getElementById(this.id).className += ' active';
+    } else {
+        document.getElementById(idAux).classList.add('active');
+        document.getElementById(idAux + '-1').className += ' active';
+    }
+    loadData(data, idAux);
+}
 
 function loadData(data, criterio) {
     let html = '';
